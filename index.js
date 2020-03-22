d7637f28adeb8eefdc872928ff50038f24a55843
Hua Luo <hluo@lifesize.com>
2020-03-22 18:08:17 -0500

    auth still false

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto');
 const urlencode = require('urlencode');
+const moment = require('moment');
 
 const TENCENT_SECRET_ID = 'AKIDNccOxHLLagcW9iNILFxEG4OTVUZQjWjj';
 const TENCENT_SECRET_KEY = 'uZzGI5dw2tU17A6pqwoWyAFBtMMDyUM7';
@@ -7,19 +8,16 @@ const TENCENT_SECRET_KEY = 'uZzGI5dw2tU17A6pqwoWyAFBtMMDyUM7';
 //const url = 'http://cmq-topic-sh.api.tencentyun.com'; //internal
 let url = 'https://cmq-topic-sh.api.qcloud.com/v2/index.php?'; // external
 const axios = require('axios');
-// https://domain/v2/index.php?Action=PublishMessage
-// &topicName=test-topic-123
-// &msgBody=helloworld
-// &<公共请求参数></公共请求参数>
-
 //Action
 //Nonce
 //SecretId
 //Timestamp
 //msgBody
 //topicName
+const nowUnix = moment().unix();
+console.log('nowUnix:' + nowUnix);
 
-url = url + 'Action=PublishMessage&Nonce=1584917647&SecretId=' + TENCENT_SECRET_ID + '&Timestamp=1584917647';
+url = url + 'Action=PublishMessage&Nonce=' + nowUnix + '&SecretId=' + TENCENT_SECRET_ID + '&Timestamp=' + nowUnix;
 url = url + '&msgBody=hello2&topicName=event_bus';
 
 const preSignString1 = 'GET' + url;
@@ -30,7 +28,6 @@ const preSignString2 = hmac.digest('base64');
 console.log('sign encode:' + preSignString2);
 const preSignString3 = urlencode(preSignString2);
 console.log('url encode:' + preSignString3);
-
 url = url + '&Signature=' + preSignString3;
 axios.get(url).then(function (response) {
     console.log(response);
